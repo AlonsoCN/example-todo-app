0bd7741b31db884fb427e8a5f23bc953b3bef61c
Alonso Calle Núñez <luis.alonso.cn@gmail.com>
2017-05-11 18:14:42 -0500

    - Created implemented - Toggle pending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,25 +16,54 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._todoDataService.getAllTodos().subscribe(
-      result => this.todos = result
-    );
-  }
-
-  addTodo() {
-    this._todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo(null);
-  }
-
-  toggleTodoComplete(todo) {
-    // this._todoDataService.toggleTodoComplete(todo);
-  }
-
-  removeTodo(todo) {
-    // this._todoDataService.deleteTodoById(todo.id);
+    this.getAllTodos();
   }
 
   getAllTodos() {
-    // return this._todoDataService.getAllTodos();
+    this._todoDataService.getAll()
+        .subscribe(result => this.todos = result);
+  }
+
+  addTodo() {
+    // We don't want to add blank todos without titles
+    if (this.newTodo.title) {
+      this._todoDataService.add(this.newTodo)
+        .subscribe(result => {
+          this.todos.push(result);
+        });
+    }
+    this.newTodo = new Todo(null);
+  }
+
+  toggleTodoComplete(todo: Todo) {
+    const isUpdated = this._todoDataService.toggleTodoComplete(todo);
+    setTimeout((isUpdated) => {
+      console.log('updated', isUpdated);
+    }, 3000);
+
+    if (isUpdated) {
+      const todoUpdated = this.updateTodoById(todo.id, {
+        complete: !todo.complete
+      });
+      console.log(todoUpdated);
+      return todoUpdated;
+    }
+  }
+
+  removeTodo(todo: Todo) {
+    // this._todoDataService.deleteTodoById(todo.id);
+  }
+
+  updateTodoById(id: number, values: Object = {}) {
+    const todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  private getTodoById(id: number): Todo {
+    return this.todos.filter(todo => todo.id === id).pop();
   }
 }
--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -1,55 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo';
 
-import { Http, Response } from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class TodoDataService {
-  private TODO_URL = 'assets/api/todos.json';
+  private TODO_URL = 'http://localhost:8080/api/todos';
   private lastId = 0;
+
+  headers = new Headers({ 'Content-Type': 'application/json' });
+  options = new RequestOptions({ headers: this.headers });
 
   constructor(public _http: Http) {
   }
 
-  addTodo(todo: Todo): any {
-    if (!todo) {
-      return false;
-    }
-    return todo;
+  getAll(): Observable<Todo[]> {
+      return this._http.get(this.TODO_URL)
+        .map(result => result.json().map(i => new Todo(i)));
   }
 
-  deleteTodoById(id: number) {
+  add(todo: Todo): Observable<Todo> {
+    const data = {
+      title: todo.title,
+      complete: todo.complete
+    };
+    return this._http.post(this.TODO_URL, data, this.options)
+      .map(result => new Todo(result.json()));
+  }
+
+  private updateById(id: Number, values: Object = {}): Observable<{ status: Boolean }> {
+    values['id'] = id;
+    return this._http.put(this.TODO_URL, values, this.options)
+      .map(result => result.json());
+  }
+
+  delete(todo: Todo) {
+    const todoId = todo.id;
     // this.todos = this.todos.filter(todo => todo.id !== id);
     return true;
   }
 
-  updateTodoById(id: number, values: Object = {}) {
-    // const todo = this.getTodoById(id);
-    // if (!todo) {
-    //   return null;
-    // }
-    // Object.assign(todo, values);
-    // return todo;
-  }
-
-  getAllTodos() {
-      // return this.todos;
-      return this._http.get(this.TODO_URL)
-                .map(result => result.json().todos.map(i => new Todo(i)));
-  }
-
-  getTodoById(id: number) {
-    // return this.todos.filter(todo => todo.id === id).pop();
-  }
-
-  toggleTodoComplete(todo: Todo) {
-    const updateTodo = this.updateTodoById(todo.id, {
+  toggleTodoComplete(todo: Todo): Boolean {
+    const updateTodo = this.updateById(todo.id, {
       complete: !todo.complete
     });
-    return updateTodo;
+    let response;
+    updateTodo.subscribe(result => response = result.status);
+    return response;
   }
-
 }
